f0030844e214e9663f027ef3feec0b53ae60b6f8
Ryli Davis <rylidavis94@gmail.com>
2022-07-14 16:08:57 -0700

    Fetching the flickr api

--- src/components/App.js
+++ src/components/App.js
@@ -1,19 +1,57 @@
-import React from "react";
+import React, { Component } from "react";
 import "../css/index.css";
 
 //components
-import PhotoGallery from "./PhotoGallery";
-import SearchForm from "./SearchForm";
 import Nav from "./Nav";
+import apiKey from "../config";
+import SearchForm from "./SearchForm";
 
-const App = () => {
-  return (
-    <div className="container">
-      <SearchForm />
-      <Nav />
-      <PhotoGallery />
-    </div>
-  );
-};
+
+class App extends Component {
+
+
+  constructor() {
+    super();
+        this.state = {
+          photos: []
+        }
+  } 
+
+  componentDidMount() {
+/*flickr api endpoint is https://api.flickr.com/services
+I went to the flickr api explorer page here https://www.flickr.com/services/api/
+and I clicked on the flickr.photos.search in 'photos' in 'API Methods' and got my
+url. rest/?method=flickr.photos.search&api_key=${apiKey}&text=fun&per_page=16&format=json&nojsoncallback=1
+*/
+
+
+    fetch(`https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&text=travel&max_upload_date=07%2F01%2F2022&
+    &per_page=16&format=json&nojsoncallback=1`)
+    .then(res => res.json())
+    .then(data => {
+        this.setState({
+          photos: data.photos.photo
+        })
+    })
+    .catch(error => {
+      console.log('Error fetching and parsing data', error);
+    });
+  }
+
+
+
+  render() {
+    console.log(this.state.photos)
+    return (
+      <div className="container">
+        <SearchForm />
+        <Nav apiKey={apiKey} photos={ {travel: this.state.photos, penguins:this.state.photos, flowers: this.state.photos}}/>
+
+
+
+      </div>
+    );
+  }
+}
 
 export default App;
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,19 +1,31 @@
 import React from "react";
+import { NavLink, Routes, Route } from "react-router-dom";
+import PhotoGallery from "./PhotoGallery";
 
-const Nav = () => {
+const Nav = ({apiKey, photos}) => {
   return (
     <nav className="main-nav">
       <ul>
         <li>
-          <a href="#">Cats</a>
+        
+          <NavLink to="/Travel">Travel</NavLink>
         </li>
         <li>
-          <a href="#">Dogs</a>
+          <NavLink to="/Penguins">Penguins</NavLink>
         </li>
         <li>
-          <a href="#">Computers</a>
+          <NavLink to="/Flowers">Flowers</NavLink>
+        
         </li>
       </ul>
+<Routes>
+<Route path="/" element={<PhotoGallery apiKey={apiKey} photos={photos.travel} />} />
+<Route path="/travel" element={<PhotoGallery apiKey={apiKey} photos={photos.travel} />} />
+<Route path="/penguins" element={<PhotoGallery apiKey={apiKey} photos={photos.penguins} />} />
+<Route path="/flowers" element={<PhotoGallery apiKey={apiKey} photos={photos.flowers}/>} />
+
+</Routes>
+
     </nav>
   );
 };
